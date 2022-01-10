09968e6bb4350eacbe866a5c267bbcc6355b0f71
DiegoAsmat12 <diegoas1205@gmail.com>
2022-01-10 15:39:35 -0500

    Agregando evento en el formulario con "event"

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
+let formulario = document.querySelector('form')
 
+formulario.addEventListener('submit')
+
+function enviarFormulario(elementForm){
+    elementForm.preventDefault();
+    console.log(elementForm.target);
+    let nombre = document.querySelector('#nombre');
+    let edad = document.querySelector('#edad');
+    let password = document.querySelector('#password');
+
+    let informacion={
+        nombre:nombre.value,
+        edad : edad.value,
+        password: password.value
+    }
+}
